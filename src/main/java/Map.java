3283c5bd27809fe5cf1db48be5f693469f87548b
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-13 21:12:05 +0300

    feat : add project starting structure

---
+++ src/main/java/Map.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Map {
+    private final int height;
+    private final int weight;
+    private HashMap<Coordinates, Entity> entity;
+
+    public Map(int height, int weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+
+}
