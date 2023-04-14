0d14f884de4c01340d3601a277e4d7169a4bc9aa
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-14 16:34:41 +0300

    chore : import

--- src/main/java/Entity/Inanimate.java
+++ src/main/java/Entity/Inanimate.java
@@ -1,5 +1,7 @@
 package Entity;
 
+import Map.Coordinates;
+
 public abstract class Inanimate extends Entity {
     public Inanimate(Coordinates coordinates) {
         super(coordinates);
--- src/main/java/Entity/Rock.java
+++ src/main/java/Entity/Rock.java
@@ -1,5 +1,7 @@
 package Entity;
 
+import Map.Coordinates;
+
 public class Rock extends Inanimate{
     public Rock(Coordinates coordinates) {
         super(coordinates);
