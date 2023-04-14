42d813a3101546a3b1248e7abcffa1c1b2f4db7e
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-14 19:11:45 +0300

    feat : add Deer

---
+++ src/main/java/Entity/Creatures/Herbivores/Deer.java
@@ -0,0 +1,14 @@
+package Entity.Creatures.Herbivores;
+
+import Map.Coordinates;
+
+public class Deer extends Herbivore{
+    public Deer(int hp, int speed, Coordinates coordinates, int maxHp) {
+        super(hp, speed, coordinates, maxHp);
+    }
+
+    @Override
+    public void makeMove() {
+
+    }
+}
