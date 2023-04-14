99dc812c05ea59296e19bf7d88f7ce7f4afbedd5
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-14 18:43:11 +0300

    feat : add abstract metods Actions

--- src/main/java/Actions/InitActions/InitAction.java
+++ src/main/java/Actions/InitActions/InitAction.java
@@ -3,4 +3,5 @@ package Actions.InitActions;
 import Actions.Action;
 
 public abstract class InitAction extends Action {
+    public abstract void init();
 }
--- src/main/java/Actions/turnActions/TurnActions.java
+++ src/main/java/Actions/turnActions/TurnActions.java
@@ -3,4 +3,5 @@ package Actions.turnActions;
 import Actions.Action;
 
 public abstract class TurnActions extends Action {
+    public abstract void turn();
 }
