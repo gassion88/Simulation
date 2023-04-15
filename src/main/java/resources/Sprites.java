9bf3b595ce039ce7538062412c3b975e295cca5c
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-15 17:55:57 +0300

    feat : add sprites

---
+++ src/main/java/resources/Sprites.java
@@ -0,0 +1,9 @@
+package resources;
+
+public class Sprites {
+    public static String wolf = "\uD83D\uDC3A";
+    public static String tree = "\uD83C\uDF33";
+    public static String grass = "\uD83C\uDF44";
+    public static String deer = "\uD83E\uDD8C";
+    public static String rock = "\uD83C\uDF32";
+}
