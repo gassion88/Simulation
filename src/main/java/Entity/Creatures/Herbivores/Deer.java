1385a2c350a52ab4467d902cca3928c68a24f9bb
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-19 08:38:43 +0300

    chore :

--- src/main/java/Entity/Creatures/Herbivores/Deer.java
+++ src/main/java/Entity/Creatures/Herbivores/Deer.java
@@ -16,19 +16,4 @@ public class Deer extends Herbivore{
     public Deer(Coordinates coordinates, String sprite, Map map, int maxHp, int hp, int speed) {
         super(coordinates, sprite, map, maxHp, hp, speed);
     }
-
-    public static void main(String[] args) {
-        Map map = new Map(10,10);
-        map.setEntity(new Coordinates(1,1),  new Deer( new Coordinates(1, 1), Sprites.deer, map, 15, 10, 8));
-        map.setEntity(new Coordinates(1,2),  new Grass(new Coordinates(1, 2),  Sprites.grass, map, 5));
-
-        new MapConsoleRenderer().render(map);
-
-        Deer deer = (Deer) map.getEntity(new Coordinates(1,1));
-        deer.makeMove();
-
-        new MapConsoleRenderer().render(map);
-
-
-    }
 }
