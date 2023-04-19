42498df8e0a7e85b67a7713140f85bc3bc149cec
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-19 18:31:41 +0300

    feat : add turn action

---
+++ src/main/java/Actions/TurnActions/TurnEntityAction.java
@@ -0,0 +1,31 @@
+package Actions.TurnActions;
+
+import Entity.Creatures.Creature;
+import Entity.Entity;
+import Map.Map;
+
+import java.util.List;
+
+public class TurnEntityAction extends TurnAction{
+    Map map;
+    public TurnEntityAction(Map map) {
+        this.map = map;
+    }
+
+    @Override
+    public void turn() {
+        turnEntities(map);
+    }
+
+    private void turnEntities(Map map) {
+        List<?> entities = map.getEntityByType(Creature.class);
+
+        for (Object entity : entities) {
+            Creature creature = (Creature) entity;
+
+            creature.makeMove();
+        }
+    }
+
+
+}
