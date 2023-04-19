914a5f6073560c6c4f9adb9c3ed26fdabceb74b2
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-19 17:35:28 +0300

    feat : add PathFinder class

---
+++ src/main/java/Entity/Creatures/service/PathFinder.java
@@ -0,0 +1,12 @@
+package Entity.Creatures.service;
+
+import Map.*;
+
+import java.util.List;
+
+public class PathFinder extends AStarAlgorithm{
+
+    public static List<Node> getPath(Coordinates start, Coordinates target, Map map) {
+        return AStarAlgorithm.getPath(start, target, map);
+    }
+}
--- src/main/java/Map/MapConsoleRenderer.java
+++ src/main/java/Map/MapConsoleRenderer.java
@@ -19,7 +19,7 @@ public class MapConsoleRenderer {
 
             System.out.println(rowView);
         }
-        System.out.println("");
+        System.out.println(" ");
     }
 
     private String getEntitySpriteForEntity(Entity entity) {
