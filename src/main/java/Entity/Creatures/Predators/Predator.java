83639b073b8ee4b212e5e9275b99c1c722bd3b95
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-19 08:32:31 +0300

    feat : add go function to Predator

--- src/main/java/Entity/Creatures/Predators/Predator.java
+++ src/main/java/Entity/Creatures/Predators/Predator.java
@@ -2,9 +2,14 @@ package Entity.Creatures.Predators;
 
 import Entity.Creatures.Creature;
 import Entity.Creatures.Herbivores.Herbivore;
+import Entity.Creatures.service.AStarAlgorithm;
+import Entity.Creatures.service.Node;
 import Entity.Entity;
 import Entity.Inanimates.IEatable;
 import Map.*;
+
+import java.nio.file.Path;
+import java.util.List;
 
 public abstract class Predator extends Creature {
     public int damage;
@@ -38,6 +43,24 @@ public abstract class Predator extends Creature {
 
     @Override
     public void go(Map map) {
+        Coordinates targetCoordinates = map.getEntityByType(Herbivore.class).get(0).coordinates;
+        AStarAlgorithm aStarAlgorithm = new AStarAlgorithm(coordinates, targetCoordinates, map);
+
+        List<Node> path = aStarAlgorithm.getPath();
+
+        for (int i = 0; i < speed; i++) {
+            if (isCanInteract(Herbivore.class)) {
+                Coordinates interactEntityCoordinates = getInteractEntityCoordinates(Herbivore.class);
+                Entity entity = map.getEntity(interactEntityCoordinates);
+
+                toInteract(entity);
+                break;
+            } else {
+                map.moveEntity(coordinates, path.get(i).getCoordinates());
+            }
+
+            new MapConsoleRenderer().render(map);
+        }
 
     }
 }
