d0ca8dfe2dc0fb46328a76a5d90cb9380ee856f8
Soslan Gassiev <soslangassity04@gmail.com>
2023-04-26 23:15:17 +0300

    feat : add menu text resourses

---
+++ src/main/java/resources/Strings.java
@@ -0,0 +1,13 @@
+package resources;
+
+public class Strings {
+    public static String menuStepOne = """
+                1.Сгенерировать симуляцию.
+                2.Выйти
+                """;
+    public static String menuStepTwo = """
+                1.Рандомная генерация.
+                2.Настроить генерацию.
+                3.Выйти.
+                """;
+}
