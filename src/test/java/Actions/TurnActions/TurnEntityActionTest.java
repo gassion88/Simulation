bae0f92cabf30d2e55eb4c98e0ab4bcc619b66ac
Soslan Gassiev <soslangassity04@gmail.com>
2023-09-18 20:55:35 +0300

    test : TurnEntityAction

--- src/test/java/Actions/TurnActions/TurnEntityActionTest.java
+++ src/test/java/Actions/TurnActions/TurnEntityActionTest.java
@@ -1,4 +1,41 @@
+package Actions.TurnActions;
+
+import Actions.InitActions.SpawnEntityAction;
+import Entity.Creatures.Herbivores.Deer;
+import Entity.Factory.DeerFactory;
+import Entity.Factory.EntityFactory;
+import Entity.Factory.GrassFactory;
+import Entity.Factory.WolfFactory;
+import Entity.Inanimates.Grass;
+import Map.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import resources.Sprites;
+
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TurnEntityActionTest {
-  
+
+    static Map map;
+    TurnEntityAction turnEntityAction;
+
+    @BeforeEach
+    void preparedDate() {
+        map = new Map(10,10);
+    }
+
+    @Test
+    void checkMovedEntity() {
+        Deer deer = new Deer(new Coordinates(6, 6), Sprites.deer, map, 10, 10, 10);
+        Grass grass = new Grass(new Coordinates(1, 1), Sprites.grass, map, 10);
+        map.setEntity(deer.coordinates, deer);
+        map.setEntity(grass.coordinates, grass);
+
+        turnEntityAction = new TurnEntityAction(map);
+        turnEntityAction.turn();
+
+        assertEquals(deer.coordinates, grass.coordinates);
+    }
 }
