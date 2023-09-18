787be96a5beef948044a2f2c24aa84fb2bb651fd
Soslan Gassiev <soslangassity04@gmail.com>
2023-09-18 12:30:12 +0300

    test : add empty map creation check

--- src/test/java/Actions/InitActions/SpawnEntityActionTest.java
+++ src/test/java/Actions/InitActions/SpawnEntityActionTest.java
@@ -6,6 +6,7 @@ import Entity.Factory.GrassFactory;
 import Entity.Factory.WolfFactory;
 import Map.*;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -17,8 +18,8 @@ class SpawnEntityActionTest {
     static Map map;
     SpawnEntityAction spawnEntityAction;
 
-    @BeforeAll
-    static void preparedDate() {
+    @BeforeEach
+    void preparedDate() {
         map = new Map(10,10);
     }
 
@@ -34,6 +35,19 @@ class SpawnEntityActionTest {
         spawnEntityAction.init();
 
         assertEquals(map.getAllEntity().size(), 9);
+    }
+
+    @Test
+    void checkEmptyMapCreation() {
+        HashMap<EntityFactory, Integer> entityAndHerProbabilitySpawn = new HashMap<>();
+        entityAndHerProbabilitySpawn.put(new WolfFactory(), 0);
+        entityAndHerProbabilitySpawn.put(new DeerFactory(), 0);
+        entityAndHerProbabilitySpawn.put(new GrassFactory(), 0);
+
+        spawnEntityAction = new SpawnEntityAction(entityAndHerProbabilitySpawn, map);
+        spawnEntityAction.init();
+
+        assertEquals(map.getAllEntity().size(), 0);
     }
 
 }
